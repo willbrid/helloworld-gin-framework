f30bfa015d93abb20beeeeb3556e511cc63794d0
willbrid <ngaswilly77@gmail.com>
2023-08-06 15:57:19 +0100

    feat: clear project

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,14 +2,7 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func IndexHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello World",
-	})
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/", IndexHandler)
 	router.Run()
 }
